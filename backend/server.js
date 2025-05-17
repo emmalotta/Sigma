2191face2601839f631f978a76d38290358337e5
Emma <emmalotta9gmail.com>
2025-05-17 18:27:47 +0300

    test5

--- backend/server.js
+++ backend/server.js
@@ -259,8 +259,8 @@ app.get("*", (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
